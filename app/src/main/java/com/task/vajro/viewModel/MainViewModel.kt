af054a0f3d978073894c3b949f588258fa3ef963
shinto <shintopa@outlook.com>
2022-06-22 10:42:52 +0530

    Created repository and viewModel class.

--- app/src/main/java/com/task/vajro/viewModel/MainViewModel.kt
+++ app/src/main/java/com/task/vajro/viewModel/MainViewModel.kt
@@ -3,14 +3,25 @@ package com.task.vajro.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.task.vajro.model.getProductList.ProductList
+import com.task.vajro.repository.MockRepository
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val repository: MockRepository) : ViewModel() {
 
-    val mockResponseLiveData: MutableLiveData<Response<List<ProductList>>> =  MutableLiveData()
+    val mockResponseLiveData: MutableLiveData<Response<List<ProductList>>> = MutableLiveData()
 
+    init {
+        suspend {
+            mockLiveData()
+        }
+    }
     suspend fun mockLiveData() {
-
+        viewModelScope.launch {
+            val responseRepo = repository.mockApiRepository()
+            mockResponseLiveData.value = responseRepo
+        }
     }
 }
